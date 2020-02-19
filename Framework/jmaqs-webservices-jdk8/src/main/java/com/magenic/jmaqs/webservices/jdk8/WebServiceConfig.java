ac391a5ad343710dc1b8ce1e26df90d290fbcdff
Jason Edstrom <JasonE@magenic.com>
2020-02-18 21:34:48 -0600

    Checkstyle and formatting
    Module [JMAQS]:
      * SQLProvider.java
      * SQLiteProvider.java
      * PerfTimerCollection.java
      * WebServiceConfig.java
      * WebServiceDriver.java
      * WebServiceTestObject.java
      * WebServiceUtilities.java
      * DatabaseDriver.java
      * maqs_checks.xml

--- Framework/jmaqs-webservices-jdk8/src/main/java/com/magenic/jmaqs/webservices/jdk8/WebServiceConfig.java
+++ Framework/jmaqs-webservices-jdk8/src/main/java/com/magenic/jmaqs/webservices/jdk8/WebServiceConfig.java
@@ -23,6 +23,7 @@ public final class WebServiceConfig {
 
   /**
    * Grabs the URI for the Web Service.
+   *
    * @return A String containing the URI for the WebService to test
    */
   public static String getWebServiceUri() {
@@ -31,31 +32,30 @@ public final class WebServiceConfig {
 
   /**
    * Gets the expected time out in seconds.
+   *
    * @return an Integer containing the Time Out value for
    * the web service test, or -1 if none are configured
    */
   public static int getWebServiceTimeOut() {
-    return Integer.parseInt(Config.getValueForSection(WEBSERVICE_SECTION,
-            "WebServiceTimeout",
-            "-1"));
+    return Integer
+        .parseInt(Config.getValueForSection(WEBSERVICE_SECTION, "WebServiceTimeout", "-1"));
   }
 
   /**
    * Get if we want to use a proxy for the web driver traffic.
+   *
    * @return True if we want to use the proxy
    */
   public static boolean getUseProxy() {
-    return Config.getValueForSection(ConfigSection.WebServiceMaqs,
-            "UseProxy",
-            "No").equals("Yes");
+    return Config.getValueForSection(ConfigSection.WebServiceMaqs, "UseProxy", "No").equals("Yes");
   }
 
   /**
    * Get the proxy address to use.
+   *
    * @return The proxy address
    */
   public static String getProxyAddress() {
-    return Config.getValueForSection(ConfigSection.WebServiceMaqs,
-            "ProxyAddress");
+    return Config.getValueForSection(ConfigSection.WebServiceMaqs, "ProxyAddress");
   }
 }
