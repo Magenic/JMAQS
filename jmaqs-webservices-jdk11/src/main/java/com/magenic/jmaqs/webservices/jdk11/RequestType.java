148ef0a03e0b27ceed63d22693fb2a107ed21d34
bradyrad <bradyr@magenic.com>
2020-09-14 13:21:05 -0500

    -Added Request Type enum class instead of importing one -Modified WebServiceDriver to use the new Request Type enum

---
+++ jmaqs-webservices-jdk11/src/main/java/com/magenic/jmaqs/webservices/jdk11/RequestType.java
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2020 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.jmaqs.webservices.jdk11;
+
+public enum RequestType {
+  GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE
+}
