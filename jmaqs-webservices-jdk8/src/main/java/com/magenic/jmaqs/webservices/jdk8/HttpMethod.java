23f62dbddef8c6dd9e973f0c758f1366cd17fce7
Jason Edstrom <JasonE@magenic.com>
2021-08-19 14:54:56 -0500

    Removed WIP profile and added JDK11 module into build

---
+++ jmaqs-webservices-jdk8/src/main/java/com/magenic/jmaqs/webservices/jdk8/HttpMethod.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright 2021 (C) Magenic, All rights Reserved
+ */
+
+package com.magenic.jmaqs.webservices.jdk8;
+
+public enum HttpMethod {
+    POST,
+    GET,
+    PUT,
+    DELETE,
+    PATCH;
+}
